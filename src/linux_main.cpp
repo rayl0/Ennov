f22d6baa8a468569c98ccee455b2bb091d69a35e
Rajat <rajat.vx@gmail.com>
2020-01-06 09:22:44 +0530

    Added modern opengl context

--- src/linux_main.cpp
+++ src/linux_main.cpp
@@ -4,9 +4,31 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#include <GL/glx.h>
+
 #define global_variable static
-#define local_presist static
+#define local_persist static
 #define internal static
+
+typedef GLXContext (*glXCreateContextAttribsARBProc)(Display*, GLXFBConfig, GLXContext, Bool, const int*);
+
+// TODO(Rajat): Build a software renderer later
+
+global_variable GLint GlxAttributes[] =
+{
+	    GLX_X_RENDERABLE    , True,
+		GLX_DRAWABLE_TYPE   , GLX_WINDOW_BIT,
+		GLX_RENDER_TYPE     , GLX_RGBA_BIT,
+		GLX_X_VISUAL_TYPE   , GLX_TRUE_COLOR,
+		GLX_RED_SIZE        , 8,
+		GLX_GREEN_SIZE      , 8,
+		GLX_BLUE_SIZE       , 8,
+		GLX_ALPHA_SIZE      , 8,
+		GLX_DEPTH_SIZE      , 24,
+		GLX_STENCIL_SIZE    , 8,
+		GLX_DOUBLEBUFFER    , True,
+		None
+};
 
 int main(int argc, char* argv[])
 {
@@ -14,8 +36,40 @@ int main(int argc, char* argv[])
     Screen* Screen_ = DefaultScreenOfDisplay(Display_);
     int ScreenNumber = DefaultScreen(Display_);
 
-    Window Window_ = XCreateSimpleWindow(Display_, RootWindowOfScreen(Screen_), 50, 50, 600, 400, 5, 
-    	                   BlackPixel(Display_, ScreenNumber), WhitePixel(Display_, ScreenNumber));
+    int FBcount;
+    GLXFBConfig* FbC = glXChooseFBConfig(Display_, ScreenNumber, 
+    	                GlxAttributes, &FBcount);
+
+    XVisualInfo* Visual = glXGetVisualFromFBConfig(Display_, FbC[0]);
+
+    XSetWindowAttributes WindowAttribs;
+    WindowAttribs.border_pixel = BlackPixel(Display_, ScreenNumber);
+    WindowAttribs.background_pixel = WhitePixel(Display_, ScreenNumber);
+    WindowAttribs.override_redirect = true;
+    WindowAttribs.colormap = XCreateColormap(Display_, RootWindow(Display_, ScreenNumber), 
+    	                                    Visual->visual, AllocNone);
+    WindowAttribs.event_mask = ExposureMask;
+
+    Window Window_ = XCreateWindow(Display_, RootWindowOfScreen(Screen_), 50, 50, 600, 400, 5, 
+    	                   Visual->depth, InputOutput, Visual->visual, CWBackPixel | CWColormap | CWBorderPixel | CWEventMask, &WindowAttribs);
+
+    glXCreateContextAttribsARBProc glXCreateContextAttribsARB = 0;
+	glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc) glXGetProcAddressARB( (const GLubyte *) "glXCreateContextAttribsARB" );
+
+
+	int ContextAttribs[] = {
+		GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
+		GLX_CONTEXT_MINOR_VERSION_ARB, 2,
+		GLX_CONTEXT_FLAGS_ARB, GLX_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
+		None
+	};
+
+	GLXContext Context = 0;
+	Context = glXCreateContextAttribsARB(Display_, FbC[0], 0, true, ContextAttribs);
+
+	XSync(Display_, False);
+
+	glXMakeCurrent(Display_, Window_, Context);
 
     XStoreName(Display_, Window_, "Hello X11!");
 
@@ -41,7 +95,7 @@ int main(int argc, char* argv[])
     	switch(Event.type)
     	{
     		case Expose:
-    			local_presist XWindowAttributes WindowAttribs;
+    			local_persist XWindowAttributes WindowAttribs;
     			XGetWindowAttributes(Display_, Window_, &WindowAttribs);
     			printf("Window resized: (%i, %i)\n", WindowAttribs.width, WindowAttribs.height);
     			break;
@@ -79,12 +133,14 @@ int main(int argc, char* argv[])
             	printf("Pointer out of the window\n");
             	break;
             case MotionNotify:
-            	local_presist int x, y;
+            	local_persist int x, y;
             	x = Event.xmotion.x;
             	y = Event.xmotion.y;
             	printf("Pointer at (%i, %i) \n", x, y);
             	break;
     	}
+
+    	glXSwapBuffers(Display_, Window_);
     }
 
     XDestroyWindow(Display_, Window_);
